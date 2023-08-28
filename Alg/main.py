3f7434ac71aa9a555338261ede9d7599a79886c0
leusme <81135021+leusme@users.noreply.github.com>
2023-08-27 21:15:34 -0300

    Update main.py
    mudando import da base de dados

--- Alg/main.py
+++ Alg/main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-url = "https://www.inf.pucrs.br/~danielc/peng1a/outros/distancias.xls"
-df = pd.read_excel(url)
+file_path = '/content/distancias.xls'
+df = pd.read_excel(file_path)
+
+print(df.head())
 
 vertices = list(df.columns)
 
+
 graph = {vertex: {} for vertex in vertices}
+
 
 for i, vertex1 in enumerate(vertices):
     for j, vertex2 in enumerate(vertices):
@@ -14,13 +18,51 @@ for i, vertex1 in enumerate(vertices):
             graph[vertex1][vertex2] = distance
             graph[vertex2][vertex1] = distance
 
+
+def find_min_vertex(distances, visited):
+    min_vertex = None
+    min_distance = float('inf')
+    
+    for vertex in distances:
+        if not visited[vertex] and distances[vertex] < min_distance:
+            min_vertex = vertex
+            min_distance = distances[vertex]
+    
+    return min_vertex
+
+
 def prim(graph):
     start_vertex = vertices[0]
     visited = {vertex: False for vertex in vertices}
     distances = {vertex: float('inf') for vertex in vertices}
     parent = {vertex: None for vertex in vertices}
-
+    
     distances[start_vertex] = 0
+    
+    for _ in range(len(vertices)):
+        current_vertex = find_min_vertex(distances, visited)
+        visited[current_vertex] = True
+        
+        for neighbor, weight in graph[current_vertex].items():
+            if not visited[neighbor] and weight < distances[neighbor]:
+                distances[neighbor] = weight
+                parent[neighbor] = current_vertex
+    
+    mst = {vertex: {} for vertex in vertices}
+    for vertex, par in parent.items():
+        if par is not None:
+            mst[vertex][par] = distances[vertex]
+            mst[par][vertex] = distances[vertex]
+    
+    return mst
+
+
+minimum_spanning_tree = prim(graph)
+
+
+for vertex, neighbors in minimum_spanning_tree.items():
+    for neighbor, weight in neighbors.items():
+        print(f"{vertex} - {neighbor}: {weight}")
 
     for _ in range(len(vertices)):
         current_vertex = find_min_vertex(distances, visited)
